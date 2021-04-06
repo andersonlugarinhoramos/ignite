4be47754fbffabbb1dc989966898d312b6a323c4
Anderson <anderson.ramlug@gmail.com>
2021-04-05 21:51:31 -0300

    add ensureAuthenticate all routes

--- nodejs/rentx/src/shared/infra/http/routes/categories.routes.ts
+++ nodejs/rentx/src/shared/infra/http/routes/categories.routes.ts
@@ -5,6 +5,7 @@ import { CreateCategoryController } from "@modules/cars/useCases/createCategory/
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const categoriesRoutes = Router();
@@ -17,14 +18,19 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.use(ensureAuthenticated);
+categoriesRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle
+);
 
-categoriesRoutes.post("/", createCategoryController.handle);
-
-categoriesRoutes.get("/", listCategoriesController.handle);
+categoriesRoutes.get("/", ensureAuthenticated, listCategoriesController.handle);
 
 categoriesRoutes.post(
   "/import",
+  ensureAuthenticated,
+  ensureAdmin,
   upload.single("file"),
   importCategoryController.handle
 );
