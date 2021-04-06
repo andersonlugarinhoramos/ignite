db3e7ccb1364f4406483ceebb88f298ba357ab37
Anderson <anderson.ramlug@gmail.com>
2021-04-05 21:29:06 -0300

    seed user admin

--- nodejs/rentx/src/shared/infra/http/server.ts
+++ nodejs/rentx/src/shared/infra/http/server.ts
@@ -2,15 +2,15 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "@shared/infra/typeorm";
-
 import "@shared/container";
 
 import { AppError } from "@shared/errors/AppError";
 import { router } from "@shared/infra/http/routes";
+import createConnecton from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
 
+createConnecton();
 const app = express();
 
 app.use(express.json());
